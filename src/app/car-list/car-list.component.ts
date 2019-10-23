5eebff49d7a014ffc5189fb8e448f5fd5205a00f
David Santacoloma <adsi1407@gmail.com>
2019-10-22 23:33:33 -0500

    Gral: Mostrar los propietarios de los vehículos en el listado de vehículos

--- src/app/car-list/car-list.component.ts
+++ src/app/car-list/car-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CarService } from '../shared/car/car.service';
 import { GiphyService } from '../shared/giphy/giphy.service';
+import { OwnerService } from '../shared/owner/owner.service';
 
 @Component({
   selector: 'app-car-list',
@@ -9,14 +10,26 @@ import { GiphyService } from '../shared/giphy/giphy.service';
 })
 export class CarListComponent implements OnInit {
   cars: Array<any>;
+  owners: Array<any>;
 
-  constructor(private carService: CarService, private giphyService: GiphyService) { }
+  constructor(private carService: CarService, private giphyService: GiphyService, private ownerService: OwnerService) { }
 
   ngOnInit() {
     this.carService.getAll().subscribe(data => {
       this.cars = data;
       for (const car of this.cars) {
         this.giphyService.get(car.name).subscribe(url => car.giphyUrl = url);
+      }
+    });
+
+    this.ownerService.getAll().subscribe(data => {
+      this.owners = data._embedded.owners;
+      for (let i = 0; i < this.cars.length; i++) {
+        for (let j = 0; j < this.owners.length; j++) {
+          if(this.cars[i].ownerDni == this.owners[j].dni) {
+            this.cars[i].owner = this.owners[j];
+          }
+        }
       }
     });
   }
